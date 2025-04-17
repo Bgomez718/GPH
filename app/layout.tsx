ae2850edb972ac064e5c394743d6714b57e23bb2
Bryan Gomez <bgomez718@gmail.com>
2025-04-17 15:19:47 -0400

    added MetaData to preven deployment errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 
+export const metadata: Metadata = {
+  title: "Gomez Private Housing",
+  description: "A Dashboard for reviewing Gomez House Private assets",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
